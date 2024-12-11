3629278559de479ce77c7384afe43a7dacefaa28
Shehram Tahir <shehram.tahir@A006-01381.local>
2024-12-12 04:14:12 +0500

    FEAT: Added google bucket

---
+++ gbucket.py
@@ -0,0 +1,22 @@
+import time
+
+from google.cloud import storage
+from google.oauth2 import service_account
+from backend_common.common_config import CONF
+
+
+def get_google_cloud_bucket_conn(bucket_name: str):
+    """Setup Google Cloud Storage client"""
+    credentials = service_account.Credentials.from_service_account_file(CONF.google_bucket_credentials_json_path)
+    storage_client = storage.Client(credentials=credentials)
+    bucket = storage_client.bucket(bucket_name)
+    return bucket
+
+
+def upload_file_to_google_cloud_bucket(file):
+    """Upload file to google bucket"""
+    bucket = get_google_cloud_bucket_conn(CONF.google_product_bucket_name)
+    blob = bucket.blob(f"{CONF.google_product_bucket_path}/{file.filename}-{time.time()}")
+    blob.upload_from_file(file.file, content_type=file.content_type)
+    # blob.make_public()
+    return blob.public_url
