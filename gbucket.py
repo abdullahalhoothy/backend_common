069a9700ae754f059b78c6f097bf630aab754aef
abdullah abbas <abdullahalhoothy92@gmail.com>
2025-01-29 15:14:39 -0500

    added delete thumbnail

--- gbucket.py
+++ gbucket.py
@@ -1,7 +1,10 @@
 import time
-
+import logging
 from google.cloud import storage
 from google.oauth2 import service_account
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_google_cloud_bucket_conn(bucket_name: str, cred_path: str):
@@ -19,3 +22,29 @@ def upload_file_to_google_cloud_bucket(file, bucket_name, bucket_path, cred_path
     blob.upload_from_file(file.file, content_type=file.content_type)
     # blob.make_public()
     return blob.public_url
+
+
+def upload_file_to_google_cloud_bucket(file, bucket_name, bucket_path, cred_path):
+    """Upload file to google bucket"""
+    bucket = get_google_cloud_bucket_conn(bucket_name, cred_path)
+    blob = bucket.blob(f"{bucket_path}/{time.time()}-{file.filename}")
+    blob.upload_from_file(file.file, content_type=file.content_type)
+    # blob.make_public()
+    return blob.public_url
+
+
+def delete_file_from_google_cloud_bucket(
+    file_path: str, bucket_name: str, cred_path: str
+):
+    """Delete a file from a Google Cloud Storage bucket"""
+    try:
+        # Get the bucket connection
+        bucket = get_google_cloud_bucket_conn(bucket_name, cred_path)
+        blob = bucket.blob(file_path)
+        blob.delete()
+
+        return f"File {file_path} deleted successfully from bucket {bucket_name}."
+
+    except Exception as e:
+        logger.error(f"Error deleting file: {str(e)}")
+        raise e
